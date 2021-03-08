2c4e0971c2a7efc15c8163172b8566132da1da0c
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-03-08 16:56:48 +0000

    create scaffold for login pages

---
+++ edu_app/lib/login/choose_user.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: ChooseUser(),
+  ));
+}
+
+class ChooseUser extends StatefulWidget {
+  @override
+  _ChooseUserState createState() => _ChooseUserState();
+}
+
+class _ChooseUserState extends State<ChooseUser> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
