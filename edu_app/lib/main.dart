c67bbff25ff32329b4359df33eec7809a665f649
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-03-08 16:53:00 +0000

    import login and new user setup routes

--- edu_app/lib/main.dart
+++ edu_app/lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:edu_app/login/loading.dart';
+import 'package:edu_app/login/choose_user.dart';
+import 'package:edu_app/login/confirm_user.dart';
+import 'package:edu_app/new_user_setup/user_colour_and _name.dart';
 
 void main() {
-  runApp(MaterialApp(
     runApp(MaterialApp(
     initialRoute: '/',
     routes: {
       '/': (context) => Loading(),
-      '/choose': (context) => Lists(),
+      '/choose': (context) => ChooseUser(),
+      '/confirm': (context) => ConfirmUser(),
+      '/new-user': (context) => NewUser(),
     },
   ));
 
