2a53917a49b6fa278d7db8c73e309f07b6e774f6
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-03-10 19:08:25 +0000

    begin building scaffold for homepage

--- edu_app/lib/main.dart
+++ edu_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:edu_app/login/loading.dart';
 import 'package:edu_app/login/choose_user.dart';
 import 'package:edu_app/login/confirm_user.dart';
 import 'package:edu_app/new_user_setup/user_colour_and _name.dart';
+import 'package:edu_app/home/homepage.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -12,6 +13,7 @@ void main() {
       '/choose': (context) => ChooseUser(),
       '/confirm': (context) => ConfirmUser(),
       '/new-user': (context) => NewUser(),
+      '/homepage': (context) => Homepage(),
     },
   ));
 }
